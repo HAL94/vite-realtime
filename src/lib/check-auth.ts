2ef4a363b9fc678bb6e859565225f062f428433d
HAL94 <hqdarwish@gmail.com>
2025-04-02 23:01:55 +0300

    minor

--- src/lib/check-auth.ts
+++ src/lib/check-auth.ts
@@ -8,20 +8,24 @@ export type GetUserSuccess = {
   name: string;
   email: string;
 };
-export async function checkAuth(): Promise<AppResponse<GetUserSuccess> | AppResponse<null>> {
+export async function checkAuth(): Promise<
+  AppResponse<GetUserSuccess> | AppResponse<null>
+> {
   try {
-    const response = await API.get<AppResponse<GetUserSuccess>>(GET("/auth/me"));
+    const response = await API.get<AppResponse<GetUserSuccess>>(
+      GET("/auth/me")
+    );
     return response.data;
-  } catch (error) {    
+  } catch (error) {
     console.error(error);
     if (error instanceof AxiosError) {
-        return error.response?.data as AppResponse<null>
+      return error.response?.data as AppResponse<null>;
     }
     return {
-        data: null,
-        success: false,
-        statusCode: undefined,
-        message: undefined
-    }
+      data: null,
+      success: false,
+      statusCode: undefined,
+      message: undefined,
+    };
   }
 }
